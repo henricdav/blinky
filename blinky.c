3700306b89680c97c71a21f8f36d385d42525b22
Henric <henricdav@gmail.com>
2016-12-18 13:16:26 +0000

    PWM done

--- blinky.c
+++ blinky.c
@@ -5,57 +5,40 @@
 
 #define CHANNELS 3
 
-void delay_ms(uint16_t count) {
-	while(count--) {
-		_delay_ms(1);
-	}
-}
-
-void delay_us(uint16_t count) {
-	while(count--) {
-		_delay_us(1);
-	}
-}
-
-int pwmLight(int duty_cycle[])
-{
-	int time_period = 100; //us
-	int on_time[CHANNELS];
-	for (int i = 0; i < CHANNELS; ++i)
-	{
-		on_time[i] = time_period * duty_cycle[i] / 100;
-	}
-	
-	PORTB = 0b00111111;
-	for (int i = 0; i < time_period; ++i)
-	{
-		if (i == on_time[0])
-			PORTB -= 10000;
-		if (i == on_time[1])
-			PORTB -= 1000;
-		if (i == on_time[2])
-			PORTB -= 100;
-		_delay_us(1);
-	}
-
-    return 0;
-}
-
+volatile uint8_t* Port[] = {&OCR0A, &OCR0B, &OCR1B};
+int Pin[] = {0, 1, 4};
 
 int main(void)
 {
-	DDRB = 0xFF; // PORTB is output, all pins
-	PORTB = 0x00; // Make pins low to start
 	
-	int duty_cycle[CHANNELS];
+	DDRB = 0xFF;
+	// Configure counter/timer0 for fast PWM on PB0 and PB1
+	TCCR0A = 3<<COM0A0 | 3<<COM0B0 | 3<<WGM00;
+	TCCR0B = 0<<WGM02 | 1<<CS00 || 0<<CS01 | 1<<CS02; // Optional; already set
+	// Configure counter/timer1 for fast PWM on PB4
+	TCCR1 = 1<<CTC1 | 1<<PWM1A | 3<<COM1A0 | 1<<CS12 | 0<<CS11 | 1<<CS10;
+	GTCCR = 1<<PWM1B | 3<<COM1B0;
+	// Interrupts on OC1A match and overflow
+	TIMSK = TIMSK | 1<<OCIE1A | 1<<TOIE1;
+	
+	int intensity[CHANNELS];
+	int _intensity[CHANNELS];
 	int cycle_length[CHANNELS];
 	int counter[CHANNELS];
 	
 	for (int i = 0; i < CHANNELS; ++i)
 	{
-		duty_cycle[i] = 20;
+		_intensity[i] = 20;
+		intensity[i] = 20;
 		cycle_length[i] = 300;
 		counter[i] = 0;
+		*Port[i] = 255;
+	}
+	_delay_ms(1000);
+	for (int i = 0; i < CHANNELS; ++i)
+	{
+		*Port[i] = 0;
+		_delay_ms(500);
 	}
 		
 	for (;;) 
@@ -64,22 +47,15 @@ int main(void)
 		{
 			if (counter[i] == cycle_length[i])
 			{
-				duty_cycle[i] = rand() % 100;
-				cycle_length[i] = 50 + rand() % 400;
+				intensity[i] = 10 + rand() % 40;
+				cycle_length[i] = 1 + rand() % 200;
 				counter[i] = 0;
+				*Port[i] = 255 - (3*intensity[i] + 7*_intensity[i])/10;
+				_intensity[i] = (3*intensity[i] + 7*_intensity[i])/10;
 			} 
 		}
-		pwmLight(duty_cycle);
-		//PORTB = 0b00111111;
-		//_delay_ms(1000);
-		//PORTB -= 10000;
-		//_delay_ms(1000);
-		//PORTB -= 1000;
-		//_delay_ms(1000);
-		//PORTB -= 100;
-		//_delay_ms(1000);
+		_delay_ms(1);
 		
-
 		for (int i = 0; i < CHANNELS; ++i)
 		{
 			++counter[i];
