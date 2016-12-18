2adab37630970dffc8b1c940131af574741c52b4
Henric <henricdav@gmail.com>
2016-12-18 09:37:07 +0000

    Now it kond of works...

--- blinky.c
+++ blinky.c
@@ -3,6 +3,8 @@
 #include <util/delay.h>
 #include <stdlib.h>
 
+#define CHANNELS 3
+
 void delay_ms(uint16_t count) {
 	while(count--) {
 		_delay_ms(1);
@@ -15,17 +17,26 @@ void delay_us(uint16_t count) {
 	}
 }
 
-int pwmLight(int duty_cycle)
+int pwmLight(int duty_cycle[])
 {
-	int time_period = 100; //us
-	int on_time = time_period * duty_cycle / 100;
-	int off_time = time_period - on_time;
+	int time_period = 200; //us
+	int on_time[CHANNELS];
+	for (int i = 0; i < CHANNELS; ++i)
+	{
+		on_time[i] = time_period * duty_cycle[i] / 100;
+	}
 	
-	PORTB = 0b00010000;
-	delay_us(on_time);
-
-	PORTB = 0b00000000;
-	delay_us(off_time);
+	PORTB = 0b00011000;
+	for (int i = 0; i < time_period; ++i)
+	{
+		if (i == on_time[0])
+			PORTB -= 10000;
+		if (i == on_time[1])
+			PORTB -= 1000;
+		//if (i == on_time[2])
+		//	PORTB -= 100;
+		_delay_us(1);
+	}
 
     return 0;
 }
@@ -35,23 +46,35 @@ int main(void)
 {
 	DDRB = 0xFF; // PORTB is output, all pins
 	PORTB = 0x00; // Make pins low to start
-
+	
+	int duty_cycle[CHANNELS];
+	int cycle_length[CHANNELS];
+	int counter[CHANNELS];
+	
+	for (int i = 0; i < CHANNELS; ++i)
+	{
+		duty_cycle[i] = 20;
+		cycle_length[i] = 300;
+		counter[i] = 0;
+	}
+		
 	for (;;) 
 	{
-		int duty_cycle = 10 + rand() % 30;
-		int cycle_length = 200 + rand() % 500;
-		for (int i = 0; i < cycle_length; ++i)
+		for (int i = 0; i < CHANNELS; ++i)
 		{
-			pwmLight(duty_cycle);
-		}	
-		
-		//for (int percentage = 0; percentage < 100; percentage++)
-		//{
-		//	for (int i = 0; i < 10; ++i)
-		//	{
-		//		pwmLight(percentage);
-		//	}
-		//}
+			if (counter[i] == cycle_length[i])
+			{
+				duty_cycle[i] = rand() % 60;
+				cycle_length[i] = 50 + rand() % 400;
+				counter[i] = 0;
+			} 
+		}
+		pwmLight(duty_cycle);
+
+		for (int i = 0; i < CHANNELS; ++i)
+		{
+			++counter[i];
+		}
 	}
 	return 0;
 }
